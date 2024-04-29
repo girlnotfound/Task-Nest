2f2e7786d63b5294ade9536c9cceb6dfaae8db95
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-04-29 12:31:21 -0700

    Created a function to handle adding a new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,20 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    // store values of the inputs
+    const taskTitle = document.getElementById("task-title").value;
+    const taskDueDate = document.getElementById("task-due-date").value;
+    const taskDescription = document.getElementById("task-description").value;
+    // add new task to task list
+    taskList.push({
+        title:taskTitle,
+        date:taskDueDate,
+        description: taskDescription,
+    })
+    // update local storage
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    // render the task list
+    renderTaskList();
 }
 
 // Todo: create a function to handle deleting a task
@@ -34,5 +47,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+   
 });
